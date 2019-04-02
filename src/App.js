a47a71e002e38343e6eb305dd1558eaf554905c7
JN Veigel <art.jnveigel@gmail.com>
2019-04-02 17:53:05 +0200

    :fire: remove initial spinny logo, add switch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
+import SignUp from './components/signup/SignUp'
+import ChooseAvatar from './components/signup/ChooseAvatar'
 
 import ScreenTooBig from './components/ScreenTooBig.js'
 
@@ -8,20 +12,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Switch>
+          <Route path="/signup/avatar" component={ChooseAvatar} />         
+          <Route exact path="/signup" component={SignUp} />
+        </Switch>
 
         <ScreenTooBig/>
       </div>
