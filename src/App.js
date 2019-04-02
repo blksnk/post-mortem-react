9942224612840f18e581b787d4de1b8f8795fffa
JN Veigel <art.jnveigel@gmail.com>
2019-04-02 19:25:43 +0200

    :sparkles: select trusted person

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import SignUp from './components/signup/SignUp'
 import ChooseAvatar from './components/signup/ChooseAvatar'
+import TrustedPerson from './components/signup/TrustedPerson'
 
 import ScreenTooBig from './components/ScreenTooBig.js'
 import OnBoarding from './components/onBoarding/OnBoarding.js'
@@ -14,7 +15,8 @@ class App extends Component {
     return (
       <div className="App">
         <Switch>
-          <Route path="/signup/avatar" component={ChooseAvatar} />         
+          <Route exact path="/signup/avatar" component={ChooseAvatar} />  
+          <Route exact path='/signup/trusted' component={TrustedPerson} />       
           <Route exact path="/signup" component={SignUp} />
        	  <Route exact path="/" component={OnBoarding}/>
         </Switch>
