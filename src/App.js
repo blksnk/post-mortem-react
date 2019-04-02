eb77309fe6f584e2605b855880fe1b58f0b676c4
JN Veigel <art.jnveigel@gmail.com>
2019-04-02 16:10:43 +0200

    :tada: initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import ScreenTooBig from './components/ScreenTooBig.js'
+
 class App extends Component {
   render() {
     return (
@@ -20,6 +22,8 @@ class App extends Component {
             Learn React
           </a>
         </header>
+
+        <ScreenTooBig/>
       </div>
     );
   }
