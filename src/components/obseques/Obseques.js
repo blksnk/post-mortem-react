0d5167592b62b9f4f91f556cfb719efc17cc7fbc
hboisgibault <h.boisgibault@gmail.com>
2019-04-03 11:48:26 +0200

    Obseques structure

--- src/components/obseques/Obseques.js
+++ src/components/obseques/Obseques.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 import style from './Obseques.module.css'
 
+const cards = [
+  {
+    title: "Les obsèques de tes rêves",
+    subtitle: "",
+    index: 0
+  },
+  {
+    title: "Cercueil de tes rêves ?",
+    subtitle: "",
+    index: 1
+  }
+]
+
 class Obseques extends Component {
     state = {  
         index: 0,
@@ -9,10 +22,41 @@ class Obseques extends Component {
     render() {
         return (  
             <section className={style.wrapper}>
-                Obseques
+              <div className={style.title}>
+                <h1>{cards[this.state.index].title}</h1>
+              </div>
+              <div className="body">
+                {{
+                  0: (
+                    <ObsequesHome />
+                  ),
+                  1: (
+                    <ObsequesCercueil />
+                  ),
+                }[this.state.index]}
+              </div>
+              <div className="footer">
+
+              </div>
             </section>
         );
     }
+}
+
+class ObsequesHome extends Component {
+  render() {
+    return (
+      <div>Obseques Home</div>
+    )
+  }
+}
+
+class ObsequesCercueil extends Component {
+  render() {
+    return (
+      <div>Obseques Cercueil</div>
+    )
+  }
 }
  
 export default Obseques;
