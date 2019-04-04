a9b96f773104992719ab7ab5c1c2dd4e82acd9f4
JN Veigel <art.jnveigel@gmail.com>
2019-04-04 14:13:43 +0200

    :construction: display and choice of organs to donate

---
+++ src/components/organes/OrganesPick.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+
+import style from './organes.module.css'
+
+import Navbar from '../Navbar'
+import OrganesCard from './OrganesCard'
+
+const placeholderImg = 'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-128.png'
+
+const organesArray = [{
+	name: 'foie',
+	image: placeholderImg,
+}, {
+	name: 'coeur',
+	image: placeholderImg,
+},{
+	name: 'poumon',
+	image: placeholderImg,
+}, {
+	name: 'reins',
+	image: placeholderImg,
+}, {
+	name: 'cornee',
+	image: placeholderImg,
+}, {
+	name: 'intestin',
+	image: placeholderImg,
+},]
+
+export default class OrganesPick extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			foie: false,
+			coeur: false,
+			poumoun: false,
+			reins: false,
+			cornee: false,
+			intestin: false,
+		}
+	}
+
+	selectAll() {
+		for (let key in this.state) {
+			this.setState( { [key]: true } )
+		}
+	}
+
+	selectComponent(name) {
+		console.log(name)
+		this.setState({[name]: !this.state[name]})
+	}
+
+	render() {
+		return (
+			<section>
+
+				<Navbar leftUrl='/organes/choisir'/>
+				<h1>
+					Don d'organe
+				</h1>
+
+				<p>
+					Bravo, tu vas pouvoir sauver des vies ! Tu n’es pas obligé de tout donner : Choisis les organes que tu acceptes de donner
+				</p>
+
+				<button onClick={() => this.selectAll()}>
+					Tout donner
+				</button>
+
+				<div className={style.itemWrapper}>
+					{organesArray.map(item => (
+						<button key={item.name} className={style.itemBtn} onClick={() => this.selectComponent(item.name)}>
+							<OrganesCard name={item.name} active={this.state[item.name]} image={item.image}/>
+						</button>
+						))}					
+				</div>
+			</section>
+		);
+	}
+}
